8f433e9c82959c21bb27a2682b342bea9c4fe3df
Michael Bonfils <bonfils.michael@protonmail.com>
2019-09-26 09:54:19 +0200

    Enable S3A for Hadoop 3.x

--- hdfs/scripts/start.sh
+++ hdfs/scripts/start.sh
@@ -2,8 +2,19 @@
 
 export HADOOP_PREFIX=/hadoop
 export HADOOP_CONF_DIR="${HADOOP_PREFIX}/etc/hadoop"
+export HADOOP_VERSION="$(cat /hadoop_version)"
 
 . "$HADOOP_PREFIX/etc/hadoop/hadoop-env.sh"
+
+if [ "${HADOOP_VERSION:0:2}" == "3." ]; then
+	echo "Enable S3A filesystem"
+	for i in $HADOOP_PREFIX/share/hadoop/tools/lib/aws-java-sdk-bundle-*.jar $HADOOP_PREFIX/share/hadoop/tools/lib/hadoop-aws-*.jar; do
+		name=$(basename $i)
+		if [ ! -f $HADOOP_PREFIX/share/hadoop/common/lib/$name ]; then
+			ln -v -s $i $HADOOP_PREFIX/share/hadoop/common/lib/$name
+		fi
+	done
+fi
 
 if [ ! -d /tmp/hadoop-root/dfs/ ]; then
 	echo "Format namenode"
